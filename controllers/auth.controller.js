1551d56dacaf859065d4a4204bc4e44dd9c37ab2
ajautz@gmail.com <ajautz@gmail.com>
2022-05-17 12:03:46 -0400

    add user input validation for signup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,26 @@
 const User = require("../models/user.model");
-const authUtil = require('../util/authentication');
+const authUtil = require("../util/authentication");
+const validation = require("../util/validation");
 
 function getSignup(req, res) {
   res.render("./customer/auth/signup");
 }
 
 async function signup(req, res, next) {
+  if (
+    !validation.userDetailsAreValid(
+      req.body.email,
+      req.body.password,
+      req.body.fullname,
+      req.body.street,
+      req.body.postal,
+      req.body.city
+    ) || !validation.userEmailsMatch(req.body.email, req.body['confirm-email'])
+  ) {
+    res.redirect("/signup");
+    return;
+  }
+
   const user = new User(
     req.body.email,
     req.body.password,
@@ -14,19 +29,27 @@ async function signup(req, res, next) {
     req.body.postal,
     req.body.city
   );
-  
+
   try {
+    const existsAlready = await user.existsAlready();
+  
+    if(existsAlready) {
+      res.redirect('/signup');
+      return;
+    }
+
     await user.signup();
-  } catch(error) {
+    
+  } catch (error) {
     next(error);
     return;
   }
 
-  res.redirect('/login');
+  res.redirect("/login");
 }
 
 function getLogin(req, res) {
-  res.render('customer/auth/login');
+  res.render("customer/auth/login");
 }
 
 async function login(req, res) {
@@ -34,34 +57,35 @@ async function login(req, res) {
   let existingUser;
   try {
     existingUser = await user.getUserWithSameEmail();
-  } catch(error) {
+  } catch (error) {
     next(error);
     return;
   }
 
-  if(!existingUser){
-    res.redirect('/login');
+  if (!existingUser) {
+    res.redirect("/login");
     return;
   }
 
   // check password
-  const passwordIsCorrect = await user.hasMatchingPassword(existingUser.password);
+  const passwordIsCorrect = await user.hasMatchingPassword(
+    existingUser.password
+  );
 
-  if(!passwordIsCorrect){
-    res.redirect('/login');
+  if (!passwordIsCorrect) {
+    res.redirect("/login");
     return;
   }
 
   // log user in, email and password checks passed
   authUtil.createUserSession(req, existingUser, function() {
-    res.redirect('/');
+    res.redirect("/");
   });
-
 }
 
 function logout(req, res) {
   authUtil.destroyUserSession(req);
-  res.redirect('/login');
+  res.redirect("/login");
 }
 
 module.exports = {
