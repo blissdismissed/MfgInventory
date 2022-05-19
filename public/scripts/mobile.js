8202b610d2625ae4f66bd2c6d99c1dc2e0b39f41
ajautz@gmail.com <ajautz@gmail.com>
2022-05-19 11:59:34 -0400

    add mobile first styling for nav menu

---
+++ public/scripts/mobile.js
@@ -0,0 +1,8 @@
+const mobileMenuButton = document.getElementById('mobile-menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+function toggleMobileMenu() {
+  
+}
+
+mobileMenuButton.addEventListener('click', toggleMobileMenu);
