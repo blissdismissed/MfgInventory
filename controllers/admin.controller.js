f133c1ffe81321c44091b55a0033cdd3a900deff
ajautz@gmail.com <ajautz@gmail.com>
2022-05-23 16:48:38 -0400

    create product model for storing products in the database.

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,20 +1,30 @@
+const Product = require("../models/product.model");
+
 function getProducts(req, res) {
-  res.render('admin/products/all-products');
-};
+  res.render("admin/products/all-products");
+}
 
 function getNewProduct(req, res) {
-  res.render('admin/products/new-product');
-};
+  res.render("admin/products/new-product");
+}
 
-function createNewProduct(req, res) {
-  console.log(req.body);
-  console.log(req.file);
-  
-  res.redirect('/admin/products');
-};
+async function createNewProduct(req, res, next) {
+  const product = new Product ({
+    ...req.body,
+    image: req.file.filename,
+  });
+  try {
+    await product.save();
+  } catch (error) {
+    next(error);
+    return;
+  }
+
+  res.redirect("/admin/products");
+}
 
 module.exports = {
   getProducts: getProducts,
   getNewProduct: getNewProduct,
-  createNewProduct: createNewProduct,
-}
+  createNewProduct: createNewProduct
+};
