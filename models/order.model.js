317975f003865321181eb6833bef1789a6450547
ajautz@gmail.com <ajautz@gmail.com>
2022-06-06 13:18:30 -0400

    develop orders page, starting with order model

--- models/order.model.js
+++ models/order.model.js
@@ -1,21 +1,36 @@
-const db = require('../data/database');
+const db = require("../data/database");
 
 class Order {
   // Status => pending, fulfilled, cancelled
-  constructor(cart, userData, status = 'pending', date, orderId) { // 2022-05-01
+  constructor(cart, userData, status = "pending", date, orderId) {
+    // 2022-05-01
     this.productData = cart;
     this.userData = userData;
     this.status = status;
     this.date = new Date(date);
     if (this.date) {
-      this.formattedDate = this.date.toLocaleDateString('en-US', {
-        weekday: 'short',
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric',
-      })
+      this.formattedDate = this.date.toLocaleDateString("en-US", {
+        weekday: "short",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+      });
     }
     this.id = orderId;
+  }
+
+  static transformOrderDocument(orderDoc) {
+    return new Order {
+      orderDoc.productData,
+      orderDoc.userData,
+      orderDoc.status,
+      orderDoc.date,
+      orderDoc._id,
+    };
+  }
+
+  static transformOrderDocuments(orderDocs) {
+    return orderDocs.map(this.transformOrderDocument);
   }
 
   save() {
@@ -26,11 +41,46 @@ class Order {
         userData: this.userData,
         productData: this.productData,
         date: new Date(),
-        status: this.status,
+        status: this.status
       };
 
-      return db.getDb().collection('orders').insertOne(orderDocument);
+      return db
+        .getDb()
+        .collection("orders")
+        .insertOne(orderDocument);
     }
+  }
+
+  static async findAll() {
+    const orders = await db
+      .getDb()
+      .collection("orders")
+      .find()
+      .toArray();
+
+    return this.transformOrderDocuments(orders);
+  }
+
+  static async findAllForUser(userId) {
+    const uid = new monogodb.ObjectId(userId);
+
+    const orders = await db
+      .getDb()
+      .collection("orders")
+      .find({ "userData._uid": uid })
+      .sort({ _uid: -1 })
+      .toArray();
+
+    return this.transformOrderDocuments(orders);
+  }
+
+  static async findById(orderId) {
+    const order = await db
+    .getDb()
+    .collection("orders")
+    .findOne({ _id: new mongodb.ObjectId(orderId) });
+
+  return this.transformOrderDocuments(order);
   }
 }
 
