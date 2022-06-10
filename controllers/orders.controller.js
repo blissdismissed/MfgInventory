7b8af990a1b28cf0602bee9e06c4a82d03c53de1
ajautz@gmail.com <ajautz@gmail.com>
2022-06-09 23:26:32 -0400

    add payment success and failure pages, implement respective routes and controller actions

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,3 +1,5 @@
+const stripe = require('stripe')('sk_test_51L8xqMCoDMUGd4JoTzQNEmBibTiXrisrHaVmcRatGcRXGJn9mIVIcLVn4yHwRNiVAxsHVCK2u20kLYtxMUO36zH800JTj7QvPR');
+
 const Order = require('../models/order.model');
 const User = require('../models/user.model');
 
@@ -34,13 +36,41 @@ async function addOrder(req, res, next) {
 
   req.session.cart = null;
 
-  res.redirect('/orders');
+  // Stripe action for checkout
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'dummy',
+          },
+          unit_amount_decimal: 10.99 
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `localhost:3000/orders/success`,
+    cancel_url: `localhost:3000/orders/failure`,
+  });
 
+  res.redirect(303, session.url);
 }
 
+function getSuccessPage(req, res) {
+  res.render('customer/orders/success');
+}
+
+function getFailurePage(req, res) {
+  res.render('customer/orders/failure');
+}
 
 
 module.exports = {
   addOrder: addOrder,
   getOrders: getOrders,
+  getSuccessPage: getSuccessPage,
+  getFailurePage: getFailurePage,
 }
--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -5,13 +5,14 @@ const ordersController = require('../controllers/orders.controller');
 const router = express.Router();
 
 
-
-
 router.post('/', ordersController.addOrder);
 
 router.get('/', ordersController.getOrders);
 
 
+router.get('/success', ordersController.getSuccessPage);
+
+router.get('/failure', ordersController.getFailurePage);
 
 
 module.exports = router;
